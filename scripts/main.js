017f24e816bb592190658e212531d5943aa7a152
danielhero <danielhero@outlook.com>
2020-03-30 13:08:29 -0500

    added a dropdown for national parks

--- scripts/main.js
+++ scripts/main.js
@@ -2,9 +2,12 @@ import { getEateries } from "./eateries/EateryProvider.js";
 import { getParks } from "./parks/ParkProvider.js";
 import { getAttractions } from "./attractions/AttractionProvider.js";
 import { AttractionSelect }  from "./attractions/AttractionsSelector.js";
+import { ParkSelect } from "./parks/ParkSelector.js";
+
 
 getEateries()
 
-getParks()
+getParks().then(ParkSelect)
 
 getAttractions().then(AttractionSelect)
+
