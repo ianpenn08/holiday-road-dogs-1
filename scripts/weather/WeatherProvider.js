f57597953ac7c44c71b9a3be3d114801ab7a0fdf
nickglovdev <nickglovdev@gmail.com>
2020-04-02 11:24:00 -0500

    Ignored the Setting files and added the weather key to the weatherProvider.

--- scripts/weather/WeatherProvider.js
+++ scripts/weather/WeatherProvider.js
@@ -1,9 +1,11 @@
+import { settings } from "../Settings.js"
+
 let weather = []
 
 export const useWeather = () => weather
-
+    
 export const getWeather = (lat, long) => {
-    return fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&units=imperial&appid=6f03413c7fb49d61ed84cd9ef21fdd00`)
+    return fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&units=imperial&appid=${settings.weatherKey}`)
     .then(response => response.json())
     .then (parseData => {
         weather = parseData.list
